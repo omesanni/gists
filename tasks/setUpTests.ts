742ab6f0219ff307a4e81f029c719eb7112d8356
ome <osanni@tempo.io>
2021-07-18 12:43:54 -0400

    update packages

--- tasks/setUpTests.ts
+++ tasks/setUpTests.ts
@@ -1,6 +1,1 @@
 require('@testing-library/jest-dom/extend-expect');
-
-const { configure } = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
-
-configure({ adapter: new Adapter() });
